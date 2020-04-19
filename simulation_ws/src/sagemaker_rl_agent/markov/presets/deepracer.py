3307a31ac0fe4485a6cb36b7cf193e28f2d0e569
David Adkins <david@davidadkins.com>
2020-04-19 10:11:31 -0400

    undo for now

--- simulation_ws/src/sagemaker_rl_agent/markov/presets/deepracer.py
+++ simulation_ws/src/sagemaker_rl_agent/markov/presets/deepracer.py
@@ -1,6 +1,6 @@
 # from rl_coach.agents.actor_critic_agent import ActorCriticAgentParameters
 # PPO
-from rl_coach.agents.actor_critic_agent import ClippedPPOAgentParameters
+from rl_coach.agents.clipped_ppo_agent import ClippedPPOAgentParameters
 from rl_coach.base_parameters import VisualizationParameters, PresetValidationParameters
 from rl_coach.core_types import TrainingSteps, EnvironmentEpisodes, EnvironmentSteps, RunPhase
 from rl_coach.environments.gym_environment import GymVectorEnvironment
